e0481baf7f465df251df0176e218fdd2e6b846e1
pc <pc@DESKTOP-CANFMKM>
2024-12-11 16:57:37 -0500

    Formula de area de un rectangulo

--- src/ec/puce/interfaces/Main.java
+++ src/ec/puce/interfaces/Main.java
@@ -3,6 +3,13 @@ package ec.puce.interfaces;
 public class Main {
 	public static void main(String[] args) {
 		System.out.println("Hola puce");
+		
+		Rectangulo rectangulo1= new Rectangulo();
+		rectangulo1.setAltura(25);
+		rectangulo1.setBase(25);
+		System.out.println("Area del Rectangulo" + rectangulo1.calcularArea());
+		
+		
 	}
 
 }
