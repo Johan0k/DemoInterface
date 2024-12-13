d52ce4aca76657fce57c6f06d42ab620c4bbae54
pc <pc@DESKTOP-CANFMKM>
2024-12-13 17:47:46 -0500

    Actu

--- src/ec/puce/interfaces/Main.java
+++ src/ec/puce/interfaces/Main.java
@@ -5,10 +5,20 @@ public class Main {
 		System.out.println("Hola puce");
 		
 		Rectangulo rectangulo1= new Rectangulo();
-		rectangulo1.setAltura(25);
-		rectangulo1.setBase(25);
-		System.out.println("Area del Rectangulo" + rectangulo1.calcularArea());
+		rectangulo1.setAltura(4);
+		rectangulo1.setBase(5);
+		System.out.println("Area del Rectangulo " + rectangulo1.calcularArea());
 		
+		
+		Triangulo triangulo1= new Triangulo();
+		triangulo1.setAltura(5);
+		triangulo1.setBase(25);
+		System.out.println("Area del Triangulo " +triangulo1.calcularArea());
+		
+		
+		Circulo circulo = new Circulo();
+        circulo.setRadio(2);
+        System.out.println("Área del círculo: " + circulo.calcularArea());
 		
 	}
 
